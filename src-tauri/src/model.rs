9f829015b9afb3c62a84b72d079f4b62c11f33ee
Shreyash Dhakate <shreyashgirdharidhakate@gmail.com>
2024-12-12 11:24:53 +0530

    Empty main branch

--- src-tauri/src/model.rs
+++
@@ -1,9 +0,0 @@
-// src-tauri/src/model.rs
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct User {
-    pub username: String,
-    pub password_hash: String,
-    pub email: String,
-}
